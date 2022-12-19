b5a0b5d944634a6afcd8ecbe5c259fc762afd3e5
Rodrigo da Silva Costa <rodrigo.costa94@yahoo.com.br>
2022-12-18 21:47:09 -0300

    botão apagar tarefas
    alguns pontos foram corrigidos mas ainda contem bugs

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 var listatarefas = [];
-var check = false
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Local storage para carregar da memoria e adicionar atributos de acordo com o indice do array
 onload = function atualizar1 (){
     var b = localStorage.getItem("buscarlista")
-    console.log(b)
+    // console.log(b)
     var c = JSON.parse(b)
     let lista = document.getElementById("listaelementos")
     // let listacompleta = document.getElementById("listacompleta")
     // var div = document.createElement("div")
 
-    console.log(c)
-    console.log(c.length)
+    // console.log(c)
+    // console.log(c.length)
 
     for ( var i = 0; i < c.length; i++){
         // console.log(c[i])
@@ -20,32 +22,74 @@ onload = function atualizar1 (){
 
         div.textContent = c[i]
         div.setAttribute("id", [i])
+        div.setAttribute("class", "elementos")
         lista.append(div)
 
         editar.textContent = "Editar"
-        editar.setAttribute("id", "editar")
+        editar.setAttribute("class", "editar")
+        editar.setAttribute("id", [i])
         div.append(editar)
 
         apagar.textContent = "Apagar"
-        apagar.setAttribute("id", "apagar")
-        div.append(apagar) 
-        
+        apagar.setAttribute("class", "apagar")
+        apagar.setAttribute("id", [i])
+        div.append(apagar)  
+              
     }
+
+    let classapagar = document.querySelectorAll(".apagar") //pega todos os botões apagar
+        classapagar.forEach(function(e){ // faz um for em todos os botoes apagar
+            e.addEventListener("click", functionapagar2 ) // adiciona o evento click em todos os botoes apagar
+        })
+
+        function functionapagar2 (evento){
+            var botaoclicado = evento.target.id
+            var listaindex = listatarefas.indexOf(listatarefas[botaoclicado]) // pega o index da string clicada
+            // console.log(listatarefas[botaoclicado]) // pega a string clicada
+
+            // console.log(listaindex)
+            // console.log(botaoclicado)
+
+            if(listaindex == botaoclicado){
+
+                let divapagada = document.getElementById(listaindex)
+                
+                let lista = document.querySelector("#listaelementos")
+
+                lista.removeChild(divapagada)
+                
+                // remove a string do array, 
+                //remove 1 elemento do índice da variavel listaindex, e adiciona null para nao perder a ordem do array
+                listatarefas.splice(listaindex, 1, null) 
+                
+                //atualiza o array de acordo com as divs apagadas
+                var salvalista = JSON.stringify(listatarefas)
+                localStorage.setItem("buscarlista", salvalista)
+
+                console.log(listatarefas)
+                // console.log(listaindex)
+            }
+
+            
+            // console.log(listaindex)
+            
+        }
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Botão criar + Adicionar atributos de acordo com o indice de cada array
 document.getElementById("botaocriar").addEventListener("click", function(){
     let textoinp = document.getElementById("input")
     let lista = document.getElementById("listaelementos")
     var div = document.createElement("div")
 
-
     let apagar = document.createElement("button")
     let editar = document.createElement("button")
-
     
         div.textContent = textoinp.value
 
-    if(textoinp.value != "" ){
+    if(textoinp.value != ""){
 
         lista.append(div)
 
@@ -59,18 +103,13 @@ document.getElementById("botaocriar").addEventListener("click", function(){
         
         listatarefas.push(textoinp.value)
 
-        for( let i = 0; i < listatarefas.length; i++){
+        for( let i = 0; i < listatarefas.length; i++){           
             div.setAttribute("id", [i])
             div.setAttribute("class", "elementos")
             apagar.setAttribute("id", [i])
             editar.setAttribute("id", [i])
         }
 
-        // let elementos = document.querySelectorAll(".elementos")
-
-        // elementos.forEach(function(element){
-
-        // })
 
         let classapagar = document.querySelectorAll(".apagar") //pega todos os botões apagar
         classapagar.forEach(function(e){ // faz um for em todos os botoes apagar
@@ -78,40 +117,44 @@ document.getElementById("botaocriar").addEventListener("click", function(){
         })
 
         function functionapagar (evento){
-            let botaoclicado = evento.target
-            // let position = listatarefas
-            let position = null
-            // position = listatarefas[botaoclicado.id]
-            // position = listatarefas.lastIndexOf([botaoclicado.id])
+            var botaoclicado = evento.target.id
 
-            for ( let i = 0; i < botaoclicado.id; i ++ ){
-                // return position.lastIndexOf()
-                position = listatarefas[botaoclicado.id[i]]
+            // console.log(listatarefas[botaoclicado]) // pega a string clicada
+            var listaindex = listatarefas.indexOf(listatarefas[botaoclicado]) // pega o index da string clicada            
+
+            // console.log(listaindex)
+            // console.log(botaoclicado)
+
+            if(listaindex == botaoclicado){
+
+                let divapagada = document.getElementById(listaindex)
+                
+                let lista = document.querySelector("#listaelementos")
+
+                lista.removeChild(divapagada)
+                
+                // remove a string do array, 
+                //remove 1 elemento do índice da variavel listaindex, e adiciona null para nao perder a ordem do array
+                listatarefas.splice(listaindex, 1, null) 
+                
+                //atualiza o array de acordo com as divs apagadas
+                var salvalista = JSON.stringify(listatarefas)
+                localStorage.setItem("buscarlista", salvalista)
+
+                console.log(listatarefas)
+                // console.log(listaindex)
+            }
+            else if (listaindex == null){
+                let apagartudo = document.getElementById(listaindex)
+
+                apagartudo.style.display = "none"
             }
 
-            console.log(botaoclicado.id)
-            // console.log(listatarefas[botaoclicado.id])
-            console.log(position)
-            if(botaoclicado.id == position){
-                // position.remove()
-                // evento.innerHTMl = ""
-            }
-        }
-
-        // document.getElementById("apagar").addEventListener("click", function(e){
             
-        //     let divclicada = e.target
-        //     // var div = document.createElement("div")
-        //     let position = listatarefas[i]
-        
-        //     // console.log(divclicada)
-        //     console.log(position)
-        
-        //     if(divclicada == position){
-        //         position.remove()
-        //     }
-        
-        // })
+            
+            // criar função de atualizar index de arrays para null
+            
+        }
 
         var salvalista = JSON.stringify(listatarefas)
         localStorage.setItem("buscarlista", salvalista)
@@ -120,15 +163,10 @@ document.getElementById("botaocriar").addEventListener("click", function(){
 
         textoinp.focus()
     }
-    else {
-        check = true
-    }
-    
-
-    
-
 
 })
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 document.getElementById("limpartudo").addEventListener("click",function(){
     let lista = document.getElementById("listaelementos")
