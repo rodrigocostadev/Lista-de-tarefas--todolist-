8355c59380ad72ab34659f76d141ddbbc6b50671
Rodrigo da Silva Costa <rodrigo.costa94@yahoo.com.br>
2023-01-03 19:38:27 -0300

    read-me
    atualização com a descrição dos bugs

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Lista-de-tarefas
  projeto de estudos para exercitar a logica basica de programação atraves de JavaScript puro.
 
+O projeto contem alguns bugs como:
+    1 Ao adicionar tarefas e atualizar a pagina, não é possivel apagar ou editar tarefas
+    2 Após apagar uma tarefa, as tarefas posteriores não podem ser editadas 
+
  Link do projeto:
  https://rodrigocostadev.github.io/Lista-de-tarefas-todolist/
+
+NOTA, POSSUI BUGS: 
+    Na função editar eu precisava pegar o id com o numero do input para conseguir manipulalo, 
+mas como ja pego id's com numeros das divs das tarefas não foi possivel, sendo assim tentei pegar o input 
+pela classe e pela tag, funciona até apagar uma tarefa, apos isso os botões de editar das tarefas posteriores 
+em relação a div que foi apagada não funcionam mais, pois a logica que usei foi a seguinte, se o id do 
+botão clicado de editar for igual ao index da classe ou da tag vai editar a tarefa, ao apagar a tarefa 
+o id do botão editar é apagado com toda a tarefa, e o index  da tag que foi apagada é substituida e nao apagada.
+    Sendo assim não encontrei outra forma de concluir o projeto.
--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var listatarefas = [];
 var arraybotaoclicado = [];
 let checkbotaoclicado = null
+//NOTA, POSSUI BUGS: 
+//Na função editar eu precisava pegar o id com o numero do input para conseguir manipulalo, 
+//mas como ja pego id's com numeros das divs das tarefas não foi possivel, sendo assim tentei pegar o input 
+//pela classe e pela tag, funciona até apagar uma tarefa, apos isso os botões de editar das tarefas posteriores 
+//em relação a div que foi apagada não funcionam mais, pois a logica que usei foi a seguinte, se o id do 
+//botão clicado de editar for igual ao index da classe ou da tag vai editar a tarefa, ao apagar a tarefa 
+//o id do botão editar é apagado com toda a tarefa, e o index  da tag que foi apagada é substituida, e nao apagada
+// Sendo assim não encontrei outra forma de concluir o projeto.
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -77,11 +85,11 @@ document.getElementById("botaocriar").addEventListener("click", function(){
             // let classtarefainput = document.getAttribute("data")[botaoclicado]
             var classtarefainput = document.getElementsByClassName("tarefainput")[botaoclicado] 
             // let classtarefainput = document.getElementsByClassName("tarefainput")  
-            let aa = classtarefainput.getAttribute("id", botaoclicado)
+            // let aa = classtarefainput.getAttribute("id", botaoclicado)
             // let classtarefainput = document.querySelectorAll(".tarefainput")[botaoclicado]
             // classtarefainput.getElementById(botaoclicado)
             // classtarefainput = botaoclicado
-            console.log(aa + "pp")      
+            // console.log(aa + "pp")      
             // console.log(diveditada.id)
             // console.log(classtarefainput.id[botaoclicado])
             // console.log(botaoclicado)
